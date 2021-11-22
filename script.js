1ab102479e6039192990691c1b4268086051901c
jpwiig <jpwiig@gmail.com>
2021-11-22 16:00:26 +0100

    JS greier'

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 alert ('funky');
+var bilder = ['./bilder/72dpi (1 of 10)', './bilder/72dpi (5 of 10)','./bilder/72dpi (6 of 10)','./bilder/72dpi (7 of 10)','./bilder/72dpi (9 of 10)','./bilder/72dpi (10 of 10)'];
+for(let i=0; i< bilder.length; i++){
+	bilder[i]=+ " ";
+
+}
+	

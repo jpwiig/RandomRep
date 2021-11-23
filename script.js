831abe157e1c6ff5be40e1f715b39a7c631ea3fa
jpwiig <jpwiig@gmail.com>
2021-11-23 20:04:36 +0100

    stuff

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 	
 
 images= [
-	"./bilder/72dpi (1 of 10)"
-	"./bilder/72dpi (5 of 10)"
-	"./bilder/72dpi (6 of 10)"
-	"./bilder/72dpi (7 of 10)"
-	"./bilder/72dpi (9 of 10)"
-	"./bilder/72dpi (10 of 10)"
+	"./bilder/72dpi (1 of 10).jpg"
+	"./bilder/72dpi (5 of 10).jpg"
+	"./bilder/72dpi (6 of 10).jpg"
+	"./bilder/72dpi (7 of 10).jpg"
+	"./bilder/72dpi (9 of 10).jpg"
+	"./bilder/72dpi (10 of 10).jpg"
 ]
 
 //document.getElementById("galleri").src=images[randInt]; 
@@ -19,7 +19,5 @@ function endreplassering(var i){
 	for(int i=0; bilder.length; i++){
 			document.getElementById(".galleri").src=bilder[i];
 	}
-
-	
 }
 }

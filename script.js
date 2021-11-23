535e7c04908e2efd49ad88d88d9e737d4d6a11b6
jpwiig <jpwiig@gmail.com>
2021-11-23 14:04:50 +0100

    push igjen

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+Object.onclick=function(){
+	var images;
+	function randInt = function (min, max){
+		return Math.floor((Math.random()*((max+1)-min))+min)
+	};
 
-var bilder = ['./bilder/72dpi (1 of 10)', './bilder/72dpi (5 of 10)','./bilder/72dpi (6 of 10)','./bilder/72dpi (7 of 10)','./bilder/72dpi (9 of 10)','./bilder/72dpi (10 of 10)'];
-for(let i=0; i< bilder.length; i++){
-
+images= [
+	"./bilder/72dpi (1 of 10)"
+	"./bilder/72dpi (5 of 10)"
+	"./bilder/72dpi (6 of 10)"
+	"./bilder/72dpi (7 of 10)"
+	"./bilder/72dpi (9 of 10)"
+	"./bilder/72dpi (10 of 10)"
+]
+document.getElementById("galleri").src=images[randInt]; 
 
 }
-	

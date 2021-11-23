9356e68da60c0f983a4df7f1292dc7916974b22d
jpwiig <jpwiig@gmail.com>
2021-11-23 21:57:19 +0100

    push push gutta

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 	var images;
 	
 
-images= [
-	"./bilder/72dpi (1 of 10).jpg"
-	"./bilder/72dpi (5 of 10).jpg"
-	"./bilder/72dpi (6 of 10).jpg"
-	"./bilder/72dpi (7 of 10).jpg"
-	"./bilder/72dpi (9 of 10).jpg"
-	"./bilder/72dpi (10 of 10).jpg"
+images.src= [
+	'./bilder/72dpi (1 of 10)'
+	'./bilder/72dpi (5 of 10)'
+	'./bilder/72dpi (6 of 10)'
+	'./bilder/72dpi (7 of 10)'
+	'./bilder/72dpi (9 of 10)'
+	'./bilder/72dpi (10 of 10)'
+	
 ]
 
 //document.getElementById("galleri").src=images[randInt]; 
@@ -19,5 +20,4 @@ function endreplassering(var i){
 	for(int i=0; bilder.length; i++){
 			document.getElementById(".galleri").src=bilder[i];
 	}
-}
 }
